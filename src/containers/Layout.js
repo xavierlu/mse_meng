ff297db7366756e8ec0bbaa4f1a229e985a30790
xavierlu <xllgms@gmail.com>
2019-01-12 01:54:11 +0800

    finished brand lockups

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,36 +1,42 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../store/actions/auth";
-import Logo from "./cornell-logo-black";
+import CE_MSE_LOGO from "./CE_MSE.png";
+import CORNELL_LOGO_RED from "./cuseal_full_red240.png";
 
-const { Header, Content, Footer } = Layout;
+const { SubMenu } = Menu;
+
+const { Header, Content, Footer, Sider } = Layout;
 
 class CustomLayout extends React.Component {
   render() {
     return (
       <Layout className="layout">
-        <Header style={{ height: "61.7px", backgroundColor: "#B31B1B" }}>
-          <Logo className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            style={{ lineHeight: "61.7px", backgroundColor: "#B31B1B" }}
-          >
-            {this.props.isAuthenticated ? (
-              <Menu.Item key="2" onClick={this.props.logout}>
-                Logout
-              </Menu.Item>
-            ) : (
-              <Menu.Item key="2">
-                <Link to="/login">Login</Link>
-              </Menu.Item>
-            )}
-          </Menu>
+        <Header style={{ height: "113px", backgroundColor: "#F7F7F7" }}>
+          <img
+            src={CORNELL_LOGO_RED}
+            height="73px"
+            style={{
+              marginTop: 20,
+              marginBottom: 20,
+              marginLeft: 10,
+              marginRight: 7
+            }}
+          />
+          <img
+            src={CE_MSE_LOGO}
+            height="73px"
+            style={{
+              marginTop: 25,
+              marginBottom: 15,
+              marginLeft: 7,
+              marginRight: 10
+            }}
+          />
         </Header>
-        <Content style={{ padding: "0 50px" }}>
+        <Content style={{ padding: "0 50px", backgroundColor: "#F7F7F7" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>
               <Link to="/">Home</Link>
@@ -39,13 +45,56 @@ class CustomLayout extends React.Component {
               <Link to={`/profiles/${this.props.userId}`}>Profile</Link>
             </Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-            {this.props.children}
-          </div>
+          <Layout style={{ padding: "24px 0", background: "#fff" }}>
+            <Sider width={200} style={{ background: "#fff" }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={["2"]}
+                defaultOpenKeys={["sub1"]}
+                style={{ height: "100%" }}
+              >
+                <SubMenu
+                  key="sub1"
+                  title={
+                    <span>
+                      <Icon type="user" />
+                      subnav 1
+                    </span>
+                  }
+                >
+                  {this.props.isAuthenticated ? (
+                    <Menu.Item key="2" onClick={this.props.logout}>
+                      Logout
+                    </Menu.Item>
+                  ) : (
+                    <Menu.Item key="2">
+                      <Link to="/login">Login</Link>
+                    </Menu.Item>
+                  )}
+                </SubMenu>
+                <SubMenu
+                  key="sub2"
+                  title={
+                    <span>
+                      <Icon type="laptop" />
+                      subnav 2
+                    </span>
+                  }
+                >
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+              {this.props.children}
+            </div>
+          </Layout>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2016 Created by Ant UED
-        </Footer>
+
+        <Footer style={{ textAlign: "center", backgroundColor: "#F7F7F7" }} />
       </Layout>
     );
   }
