ef70f80d4575082d60e48f8617e6b173cf40e5ba
Xavier Lu <xll2@cornell.edu>
2019-02-20 22:08:14 -0500

    requirement.txt

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn home.wsgi --log-file -
+web: gunicorn home.wsgi -b 0.0.0.0:$PORT
--- home/wsgi/dev.py
+++ home/wsgi/dev.py
@@ -1,9 +1,7 @@
-from dj_static import Cling
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.dev")
 
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
