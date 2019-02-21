167573509e7db611fc9b7450a04a1208ad0a8e26
Xavier Lu <xll2@cornell.edu>
2019-02-20 22:04:33 -0500

    dev.py wsgi stuff

--- home/wsgi/dev.py
+++ home/wsgi/dev.py
@@ -1,7 +1,9 @@
+from dj_static import Cling
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.dev")
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
