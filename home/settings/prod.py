e7b5357d0e26d2796f195e5700e51681f10cb7db
Xavier Lu <xll2@cornell.edu>
2019-03-06 17:43:13 -0500

    i wanna kill myself

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -6,16 +6,7 @@ import dj_database_url
 DEBUG = True
 WSGI_APPLICATION = 'home.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd484f8cj2ho7mh',
-        'USER': 'veveqgbilwrtuu',
-        'PASSWORD': '6ae606ae6161510950b79358d474105bef8ba7356d407a5440ecce375cf15465',
-        'HOST': 'ec2-54-221-231-167.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
