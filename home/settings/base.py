47ef01ab450b38b47181602bacbb6bd6fbd9ea1c
Xavier Lu <xll2@cornell.edu>
2019-03-06 17:29:17 -0500

    sdf

--- home/settings/base.py
+++ home/settings/base.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
@@ -112,3 +113,6 @@ LOGGING = {
         },
     },
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
--- home/settings/prod.py
+++ home/settings/prod.py
@@ -9,10 +9,10 @@ WSGI_APPLICATION = 'home.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd4i4akkr4li6h9',
-        'USER': 'eptfsmtlyggzvl',
-        'PASSWORD': '748a2dc44a0332771cd8c1e4a58676365073be60a89fdb798c389aa434c8a65c',
-        'HOST': 'ec2-54-204-2-25.compute-1.amazonaws.com',
+        'NAME': 'd484f8cj2ho7mh',
+        'USER': 'veveqgbilwrtuu',
+        'PASSWORD': '6ae606ae6161510950b79358d474105bef8ba7356d407a5440ecce375cf15465',
+        'HOST': 'ec2-54-221-231-167.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
