cc8df85a65b8d839d9bd3cf6a1132b051bfc2df0
Xavier Lu <xll2@cornell.edu>
2019-03-07 12:10:25 -0500

    sdfsdfsdf

--- home/settings/base.py
+++ home/settings/base.py
@@ -64,7 +64,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SITE_ID = 1
 
